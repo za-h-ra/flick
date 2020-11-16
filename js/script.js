bb8f7e35f53087c047534906cbfdfdd72c05f82e
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-16 12:26:34 -0500

    add event listener

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@ const movieSelect = document.getElementById('movie')
 
 const ticketPrice = Number(movieSelect.value)
 
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll()
+}
+
+
+
 
 
 container.addEventListener('click', (e) => {
-    console.log(e.target)
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+
+        updatedSelectedCount()
+    }
 })
