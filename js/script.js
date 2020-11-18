335d26a4d5bf8bec2f7ccba9bf5257ba1035d4c5
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-17 21:25:35 -0500

    total & count functionality

--- js/script.js
+++ js/script.js
@@ -6,11 +6,29 @@ const movieSelect = document.getElementById('movie')
 
 const ticketPrice = Number(movieSelect.value)
 
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    
+    const selectedSeatsCount = selectedSeats.length
+
+    count.innerText = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+
+
+}
+
+
 container.addEventListener('click', (e) => {
 	if (
 		e.target.classList.contains('seat') &&
 		!e.target.classList.contains('occupied')
 	) {
-		e.target.classList.toggle('selected')
+        e.target.classList.toggle('selected')
+        
+        updateSelectedCount()
 	}
 })
+
+
