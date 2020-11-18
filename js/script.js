293bf5a0fecee475d1eed1e96f696eca1a6213ab
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-17 21:06:11 -0500

    declare variables for query selectors

--- js/script.js
+++ js/script.js
@@ -3,21 +3,6 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
-
 const ticketPrice = Number(movieSelect.value)
 
-function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll()
-}
-
-
-
-
-
-container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected')
-
-        updatedSelectedCount()
-    }
-})
+console.log(typeof ticketPrice)
