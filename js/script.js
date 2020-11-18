a4628ce4389d9f66fd31dff68e2697beae63e56a
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-17 21:13:15 -0500

    create event listener for toggling seats

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
+
 const ticketPrice = Number(movieSelect.value)
 
-console.log(typeof ticketPrice)
+container.addEventListener('click', (e) => {
+	if (
+		e.target.classList.contains('seat') &&
+		!e.target.classList.contains('occupied')
+	) {
+		e.target.classList.toggle('selected')
+	}
+})
